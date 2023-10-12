a5bc12ec90e6042a5e05e66ecde49045132521d7
bishtshyam095 <L00179095@atu.ie>
2023-10-12 14:16:20 +0100

    added remaining files

---
+++ src/main/java/mavelprac/App.java
@@ -0,0 +1,13 @@
+package mavelprac;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/mavelprac/AppTest.java
@@ -0,0 +1,37 @@
+package mavelprac;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void shouldAnswerWithFalse()
+    {
+        assertFalse( false );
+    }
+    
+    // testing the Hello World file
+
+    // private App apptest;
+
+    // @Test
+    // public void Hello(){
+    //     assertEquals(apptest.main(null); "Hello World!");
+    // }
+}
